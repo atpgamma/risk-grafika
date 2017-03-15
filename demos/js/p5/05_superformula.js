00abbfed5c476c9526481732892ee2bfb13cb95e
atp <atpgamma3@tutanota.com>
2017-03-15 22:06:22 +0100

    p5 gotovi primeri

--- demos/js/p5/05_superformula.js
+++ demos/js/p5/05_superformula.js
@@ -1,36 +1,68 @@
 var radius,
   	x,
-  	y;
+  	y,
+  	a,
+  	b,
+  	m,
+  	n1,
+  	n2,
+  	n3;
+var sa, sb, sm, sn1, sn2, sn3;
 
 function setup() {
+	createCanvas(640, 480);
+	frameRate(20);
+	createP('a');
+	sa = createInput(1, 10, 1, 1);
+	createP('b');
+	sb = createInput(1, 10, 1, 1);
+	createP('m');
+	sm = createInput(1, 10, 0, 1);
+	createP('n1');
+	sn1 = createInput(1, 10, 1, 1);
+	createP('n2');
+	sn2 = createInput(1, 10, 1, 1);
+	createP('n3');
+	sn3 = createInput(1, 10, 1, 1);
+	
+	console.log('poceo');
+	angleMode(DEGREES);
 
-}
+
+}	
 
 function draw() {
+	background(0);
+	//fill(255*sin(randomGaussian(0,361)), 20, 20);
+	fill('aqua');
 	translate(width/2, height/2);
+	a = parseFloat(sa.value());
+	b = parseFloat(sb.value());
+	m = parseFloat(sm.value());
+	n1 = parseFloat(sn1.value());
+	n2 = parseFloat(sn2.value());
+	n3 = parseFloat(sn3.value());
+	
+
+
+
+
+
+	
+
 	beginShape();
-	for(var i; i < 2 * TWO_PI	; i++){
-		radius = 100 * r(i, 1, 1, 0, 1, 1, 1);
-		console.log(radius);
+	for(var i = 0; i < 360; i++){
+		//fi = (i * PI) / 180;
+		//radius = 100*r(i, 2, 3, 7, 1.8, 1, 1);
+		radius = 100 * r(i, a, b, m, n1, n2, n3);
 		x = radius * cos(i);
 		y = radius * sin(i);
 		vertex(x, y);
 	}
+	
 	endShape(CLOSE);
 }
 
 function r(t, a, b, m, n1, n2, n3) {
-	//return pow(pow(abs(cos(m * t / 4.0)), n2) + pow(abs(sin(m * t / 4.0)), n3), -1/n1);
-	var  t1,
-		 t2;
-	t1 = cos(m * t / 4.0) / a;
-	t1 = abs(t1);
-	t1 = pow(t1, n2);
-
-	t2 = sin(m * t / 4.0) / b;
-	t2 = abs(t2);
-	t2 = pow(t2, n3);
-
-
-
+	return pow(pow(abs(cos(m * t / 4.0)) / a, n2) + pow(abs(sin(m * t / 4.0)) / b, n3), -1/n1);
 }
